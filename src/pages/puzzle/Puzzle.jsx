b829b8865f908a674cbb4a564dcdf849b9b5eff4
mosh1331 <shamil.tfora@gmail.com>
2024-08-31 01:19:13 +0530

    level skip fix

--- src/pages/puzzle/Puzzle.jsx
+++ src/pages/puzzle/Puzzle.jsx
@@ -1,6 +1,6 @@
 
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import DropZone from '../../components/dropZone/DropZone';
 import DraggableBlock from '../../components/draggableBlock/DraggableBlock';
 
@@ -8,31 +8,33 @@ const puzzles = [
   {
     id: 1,
     question: "Declare a variable named `total` and set it to 10.",
-    answer: "let total = 10;",
+    answer: "let total = 10 ;",
     blocks: [
-      "let",
       "total",
       "=",
+      ";",
+      "let",
       "10",
-      ";"
+
+
     ]
   },
   {
     id: 2,
     question: "Change the value of `total` to 15.",
-    answer: "total = 15;",
+    answer: "total = 15 ;",
     blocks: [
-      "total",
       "=",
+      ";",
+      "total",
       "15",
-      ";"
     ]
   }
   // More puzzles can be added here
 ];
 
 function Puzzle({ level, onComplete }) {
-  const [currentPuzzle, setCurrentPuzzle] = useState(puzzles[level - 1]);
+  const [currentPuzzle, setCurrentPuzzle] = useState(0);
   const [code, setCode] = useState([]);
 
   const handleDrop = (block) => {
@@ -40,23 +42,31 @@ function Puzzle({ level, onComplete }) {
   };
 
   const checkSolution = () => {
-    if (code.join(' ') === currentPuzzle.answer) {
+    const joined = code.join(' ')
+    console.log(joined,'joined')
+    console.log(currentPuzzle.answer,'answer')
+    if (joined === currentPuzzle.answer) {
       alert('Correct!');
+      setCode([])
       onComplete();
     } else {
       alert('Try Again!');
     }
   };
 
+  useEffect(()=>{
+    setCurrentPuzzle(puzzles[level - 1])
+  },[level])
+
   return (
     <div className="bg-gray-800 p-6 rounded-lg shadow-md w-4/5 max-w-lg">
       <h2 className="text-2xl font-semibold mb-4">Level {level}</h2>
-      <p className="text-lg mb-6">{currentPuzzle.question}</p>
+      <p className="text-lg mb-6">{currentPuzzle?.question}</p>
       <div className="bg-gray-700 p-4 rounded mb-4">
         <DropZone onDrop={handleDrop} code={code} />
       </div>
       <div className="flex flex-wrap justify-center space-x-2 space-y-2 mb-4">
-        {currentPuzzle.blocks.map((block, index) => (
+        {currentPuzzle?.blocks?.map((block, index) => (
           <DraggableBlock key={index} block={block} />
         ))}
       </div>
